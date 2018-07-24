5b96a56472b7cc72179ff5a03d9227d55804f7cd
ROBIN ALEXANDER ROLFE <S14001664@mail.glyndwr.ac.uk>
2018-07-24 18:09:13 +0100

    Add project files.

---
+++ check 2 numbers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check_2_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x, y;
+
+
+
+            Console.WriteLine("please enter a number");//prompt user
+
+            x = Convert.ToInt32(Console.ReadLine());//get user input and assign to the variable x
+
+            Console.WriteLine("please enter a second number");//prompt user
+
+            y = Convert.ToInt32(Console.ReadLine());//get user input and assign to the variable y
+
+
+            while (x != -1)
+            {
+                if (x == y)
+                {
+                    Console.WriteLine(x + " and " + y + " are the same");
+
+                    Console.WriteLine("Lets start again..... please enter a number");//prompt user
+
+                    x = Convert.ToInt32(Console.ReadLine());//get user input and assign to the variable x
+
+                    Console.WriteLine("please enter a second number");//prompt user
+
+                    y = Convert.ToInt32(Console.ReadLine());//get user input and assign to the variable y
+                }
+
+                else
+                {
+                    Console.WriteLine(x + " and " + y + " different");
+
+                    Console.WriteLine("Lets start again..... please enter a number");//prompt user
+
+                    x = Convert.ToInt32(Console.ReadLine());//get user input and assign to the variable x
+
+                    Console.WriteLine("please enter a second number");//prompt user
+
+                    y = Convert.ToInt32(Console.ReadLine());//get user input and assign to the variable y
+                }
+                
+
+            }
+
+        }
+    }
+}
